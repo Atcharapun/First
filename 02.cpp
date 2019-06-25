053e8cfb2f9a3b701c015555d4185416f976d8ad
NJ Atcharapun <52194496+Atcharapun@users.noreply.github.com>
2019-06-25 23:17:03 +0700

    Add files via upload

---
+++ 02.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void main ()
+
+{
+	string Name = "Atcharapun Dasanom";
+	int Scores1 = 99 ;
+	int Scores2 = 98 ;
+	int Total = 179 ;
+	char Grade = 'A' ;
+	float GPA = 3.99 ;
+
+	cout << "Name : " << Name << endl;
+	cout << "Scores1 : " << Scores1 << " points" << endl;
+	cout << "Scores2 : " << Scores2 << " points" << endl;
+	cout << "Total : " << Total << endl;
+	cout << "Grade : " << Grade << endl;
+	cout << "GPA : " << GPA << endl;
+}
